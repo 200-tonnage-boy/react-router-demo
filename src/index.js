6528ac3a021b2a0dda2b9e1a9aff91324c68bfad
cjy-dell <554951030@qq.com>
2021-07-20 16:43:00 +0800

    初始化router demo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <HashRouter>
+    <div>
+      <ul>
+        <li>
+          <a href="#/a">to A</a>
+        </li>
+        <li>
+          <a href="#/b">to B</a>
+        </li>
+      </ul>
+    </div>
+    <div style={{ height: "500px", border: "1px solid" }}>
+      <Route exact path="/a">
+        <span>页面A</span>
+      </Route>
+      <Route path="/b">
+        <span>页面B</span>
+      </Route>
+    </div>
+  </HashRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
