f4233c23056202da44306651c1e905474b5ca828
cjy-dell <554951030@qq.com>
2021-07-21 22:59:15 +0800

    1.初始化my-rrd的基本到处

---
+++ src/react-router-dom/BrowserRouter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {createBrowserHistory} from 'history'
+import { Router } from 'react-router'
+class BrowserRouter extends React.Component  {
+  history = createBrowserHistory()
+  render (props) {
+    return <Router history={this.history}>
+      {this.props.children}
+      </Router>
+  }
+}
+export default BrowserRouter
---
+++ src/react-router-dom/HashRouter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {createHashHistory} from 'history'
+import { Router } from 'react-router'
+class HashRouter extends React.Component  {
+  history = createHashHistory()
+  render (props) {
+    return <Router history={this.history}>
+      {this.props.children}
+      </Router>
+  }
+}
+export default HashRouter
